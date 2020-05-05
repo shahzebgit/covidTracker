34398324e856cec7178ca706b2cb254cd329a006
shahzebgit <shahzebparkar@gmail.com>
2020-05-05 21:03:56 +0530

    Index fiel for all the components
    this is one way to get all the components through one path to import  instead of writing all the import one by one

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Cards} from './Cards/cards.components'
+export { default as Charts} from './Charts/chart.components'
+export { default as CountryPicker} from './CountryPicker/countrypicker.components'
