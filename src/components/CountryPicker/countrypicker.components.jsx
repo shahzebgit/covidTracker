40f7b9318aed292830e7ef27a83e379400811e57
shahzebgit <shahzebparkar@gmail.com>
2020-05-11 14:30:21 +0530

    Country picker dropdown
    Created Country Picker and display all the country in dropdown menu

--- src/components/CountryPicker/countrypicker.components.jsx
+++ src/components/CountryPicker/countrypicker.components.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React ,{useState ,useEffect} from 'react';
+import {NativeSelect , FormControl} from '@material-ui/core';
+import { fetchCountryData } from '../../api'
+import styles from'./countrypicker.module.css'
 
-const CountryPicker =()=>{
+const CountryPicker =({handleChange})=>{
+const [fetchedCountries ,setFetchedCountrires] = useState([]);
+
+useEffect(()=>{
+        const fetchAPI = async ()=>{
+            setFetchedCountrires(await fetchCountryData());
+        }
+
+        fetchAPI();
+    },[]);
     return(
-        <h1>CountryPicker</h1>
+        <FormControl className={styles.formControl}>
+            <NativeSelect defaultValue='' onChange={(e)=>{handleChange(e.target.value)}} >
+                <option value="global">Select the countries</option>
+    {fetchedCountries.map((c,i)=><option key={i} value={c}>{c}</option> )}
+            </NativeSelect>
+        </FormControl>
     )
 }
 
