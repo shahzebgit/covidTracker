434fe0383a84109be89fb528250734425737c85e
shahzebgit <shahzebparkar@gmail.com>
2020-05-29 09:40:48 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Cards} from './Cards/cards.components'
 export { default as Charts} from './Charts/chart.components'
 export { default as CountryPicker} from './CountryPicker/countrypicker.components'
+export {default as Footer} from './Footer/footer.components'
